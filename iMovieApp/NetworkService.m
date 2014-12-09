791377a77169d127208a1bb9ab297f1432be110d
Shayne Chow <shayne@authbuy.org>
2014-12-09 17:18:42 +0800

    解冗余：定义一个name替代原有路径path，name为json文件名,直接在return中引入name参数，解除冗余

--- iMovieApp/NetworkService.m
+++ iMovieApp/NetworkService.m
@@ -11,11 +11,12 @@
 
 @implementation NetworkService
 
-+ (id)parserData:(NSString *)path{
-    NSData *data = [NSData dataWithContentsOfFile:path];
-    //    NSString *version = [[UIDevice currentDevice] systemVersion];
++ (id)parserData:(NSString *)name{  //定义了name之后，即可将下方各数据中的path省略，直接在return中引入name参数，解除冗余
+    NSString *resoucePath = [[NSBundle mainBundle] resourcePath];   //获取包路径
+    NSString *path = [resoucePath stringByAppendingPathComponent:name]; //定义一个name替代原有路径path，name为json文件名,此时即可将原方法中的path参数换为name
     
-    //    id result = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:NULL];
+    NSData *data = [NSData dataWithContentsOfFile:path];
+    
     id result = [data objectFromJSONData];  //使用jsonKit框架提供的解析方法，替代上面原生方法
     
     return result;
@@ -23,18 +24,20 @@
 
 //测试数据解析
 + (id)testData{
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"json"];
-    NSLog(@"path: %@", path);
+//    NSString *path = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"json"];
+//    NSLog(@"path: %@", path);
+//    使用name传值，替代以上冗余部分
     
-    return [self parserData:path];
+    return [self parserData:@"test.json"];
 }
 
 //北美票房数据解析
 + (id)northUSA{
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"NorthUSA" ofType:@"json"];
-    NSLog(@"path: %@", path);
+//    NSString *path = [[NSBundle mainBundle] pathForResource:@"NorthUSA" ofType:@"json"];
+//    NSLog(@"path: %@", path);
+//    使用name传值，替代以上冗余部分
     
-    return [self parserData:path];
+    return [self parserData:@"NorthUSA.json"];
 }
 
 @end
