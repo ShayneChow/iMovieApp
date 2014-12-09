baa1f05454eeecf8fd8cf2f99c366ec74f8f7336
Shayne Chow <shayne@authbuy.org>
2014-12-09 17:05:15 +0800

    解冗余：定义私有方法，实现解析部分的复用

--- iMovieApp/NetworkService.m
+++ iMovieApp/NetworkService.m
@@ -11,25 +11,7 @@
 
 @implementation NetworkService
 
-//测试数据解析
-+ (id)testData{
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"json"];
-    NSLog(@"path: %@", path);
-    
-    NSData *data = [NSData dataWithContentsOfFile:path];
-//    NSString *version = [[UIDevice currentDevice] systemVersion];
-    
-//    id result = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:NULL];
-    id result = [data objectFromJSONData];  //使用jsonKit框架提供的解析方法，替代上面原生方法
-    
-    return result;
-}
-
-//北美票房数据解析
-+ (id)northUSA{
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"NorthUSA" ofType:@"json"];
-    NSLog(@"path: %@", path);
-    
++ (id)parserData:(NSString *)path{
     NSData *data = [NSData dataWithContentsOfFile:path];
     //    NSString *version = [[UIDevice currentDevice] systemVersion];
     
@@ -37,6 +19,22 @@
     id result = [data objectFromJSONData];  //使用jsonKit框架提供的解析方法，替代上面原生方法
     
     return result;
+}//私有方法--数据解析
+
+//测试数据解析
++ (id)testData{
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"json"];
+    NSLog(@"path: %@", path);
+    
+    return [self parserData:path];
+}
+
+//北美票房数据解析
++ (id)northUSA{
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"NorthUSA" ofType:@"json"];
+    NSLog(@"path: %@", path);
+    
+    return [self parserData:path];
 }
 
 @end
