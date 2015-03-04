194f903d6882038b84874af8978b6259a8b58259
ChowShayne <Shayne@ChowShaynedeMBP.lan>
2015-03-05 00:08:49 +0800

    修复部分 bug

--- iMovieApp/MovieCell.m
+++ iMovieApp/MovieCell.m
@@ -61,9 +61,7 @@
 
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
 
-    // Configure the view for the selected state
 }
 
 - (void)layoutSubviews{
